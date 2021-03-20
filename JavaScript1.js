0f2da7f39c9c4fa5361d639180ca4b4f5dc7e202
FevVv <luka@acmegroup.co.rs>
2021-03-20 18:14:29 +0100

    Update JavaScript1.js

--- JavaScript1.js
+++ JavaScript1.js
@@ -5,6 +5,22 @@
     } else {
         x.style.display = "none";
     }
+
+
+let username = '9c41c813-28ab-4983-8b41-b7952d47294f';
+let password = 'ecca6627-8b67-4f0f-9e29-a1349bd54aea';
+
+let headers = new Headers();
+
+//headers.append('Content-Type', 'text/json');
+headers.set('Authorization', 'Basic ' + btoa(username + ":" + password));
+
+
+fetch('https://exchange.xforce.ibmcloud.com/api/ipr/92.60.237.226', {method:'GET',
+        headers: headers,
+       })
+  .then(response => response.json())
+  .then(data => console.log(data));
 }
 
 
