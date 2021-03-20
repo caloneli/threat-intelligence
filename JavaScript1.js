a8ea8d263b8ce3c828320f8b1f751f24fc9e9ef1
ivkoneli <shadowmoister@gmail.com>
2021-03-20 17:08:02 +0100

    Promenili smo dizajn malo

--- JavaScript1.js
+++ JavaScript1.js
@@ -1,11 +1,36 @@
 ﻿function pretraziIP() {
-    var x = document.getElementById("kutije");
+    var x = document.getElementById("Text1");
     if (x.style.display === "none") {
         x.style.display = "block";
     } else {
         x.style.display = "none";
     }
 }
+function pretraziFile() {
+    var x = document.getElementById("File1");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function pretraziDomain() {
+    var x = document.getElementById("Domain1");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function pretraziHash() {
+    var x = document.getElementById("Hash1");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
 
 
 
