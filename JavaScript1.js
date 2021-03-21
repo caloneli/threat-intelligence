7977a19aa78fbeb6c8761ef99ac28cefe358bc6f
ivkoneli <shadowmoister@gmail.com>
2021-03-21 11:55:18 +0100

    Finish

--- JavaScript1.js
+++ JavaScript1.js
@@ -13,10 +13,30 @@ var ip = document.getElementById("Text1");
 var file =document.getElementById("File1");
 var domain = document.getElementById("Domain1");
 var hash = document.getElementById("Hash1");
+var rezultatDomen = document.getElementById("rezultatDomen");
+var opis= document.getElementById("Opis");
+var rezultatIP = document.getElementById("rezultat");
+var Lokacija =document.getElementById("Lokacija");
+var Razlog  = document.getElementById("Razlog");
+var rezulatHash = document.getElementById("rezulatHash");
+var rizikHash = document.getElementById("rizikHash");
+
+
 ip.style.display="none";
 file.style.display="none";
 domain.style.display="none";
 hash.style.display="none";
+
+
+rezultatDomen.style.display = "none";
+opis.style.display = "none";
+Lokacija.style.display = "none";
+Razlog.style.display = "none";
+rezultatIP.style.display = "none";
+rizikHash.style.display= "none";
+rezultatHash.style.display = "none";
+
+
 
 function pretraziIP() {
     
@@ -68,10 +88,21 @@ function Search(){
     var Input ;
 
     // Text Input 
+    
 
     if (document.getElementById("Text1").value != "")
     {
      Input = document.getElementById("Text1").value;
+     
+
+     rezultatDomen.style.display="none";
+     opis.style.display="none";
+     rezultatHash.style.display="none";
+     rizikHash.style.display="none";
+     
+     rezultatIP.style.display="block";
+     Razlog.style.display="block";
+     Lokacija.style.display="block";
   
     var api = 'https://exchange.xforce.ibmcloud.com/api/ipr/'
     var url = api + Input;
@@ -87,11 +118,12 @@ function Search(){
         document.querySelector("#rezultat").innerText = "The TI score for this IP adress :" + data.ip + " is "  
         + data.score 
         document.querySelector("#Lokacija").innerText = "The location is : " +  data.geo.country;
+        document.querySelector("#Razlog").innerText = "Reason Description : " +  data.reasonDescription;
         
 
         // OVO uzima celu datu i stavlja kao string ovako random kao na sajtu , a ovo pre toga moze da prikakze sve elemente koji nisu niz 
       //  document.querySelector("#rezultat").innerHTML = JSON.stringify(data);
-
+      document.getElementById("Text1").value = "";
 
     });
     
@@ -125,47 +157,69 @@ function Search(){
     if (document.getElementById("Domain1").value != "")
     {
         Input = document.getElementById("Domain1").value;
+        rezultatIP.style.display="none";
+        Razlog.style.display="none";
+        Lokacija.style.display="none";
+        rezultatHash.style.display ="none";
+        rizikHash.style.display ="none";
+
+
+        rezultatDomen.style.display="block";
+        opis.style.display="block";
+
      
        var api = 'https://api.xforce.ibmcloud.com/url/'
        var url = api + Input;
        fetch(url, {method:'GET', 
         headers: headers,
        })
-    .then(response => 
-    {
-         // if ok = false => error
-        if(!response.ok){
-            throw Error("Error");
-        } 
-        response.json()
-    })
-       .then(data => console.log(data));   
-       document.getElementById("rezultat").innerHTML = "Requested results for : " + Input ; 
+    .then(response =>response.json())
+    .then(data => 
+     {
+         console.log(data);   
+        document.querySelector("#rezultatDomen").innerText = "The TI score for " + Input + ": " + data.result.score;
+      /*  document.querySelector("#Lokacija").innerText = "RiskFactors of this website are : " + 
+          data.result.application.riskfactors.*/
+        document.querySelector("#Opis").innerText = "Description of the Domain : " +  data.result.application.description;
+        document.getElementById("Domain1").value = "";
+     })
     }
     
 
 
     // HASH INPUT 
 
-    if (document.getElementById("Hash1").value != ""){
+    if (document.getElementById("Hash1").value != "")
+    {
         Input = document.getElementById("Hash1").value;
-     
+
+
+        rezultatIP.style.display="none";
+        Razlog.style.display="none";
+        Lokacija.style.display="none";
+        rezultatDomen.style.display="none";
+        opis.style.display="none";
+
+        rizikHash.style.display="block";
+        rezultatHash.style.display="block";
+
+   
        var api = 'https://api.xforce.ibmcloud.com/malware/'
        var url = api + Input;
        fetch(url, {method:'GET', 
         headers: headers,
        })
-    .then(response => 
+    .then(response =>response.json()
+    )
+       .then(data => 
     {
-         // if ok = false => error
-        if(!response.ok){
-            throw Error("Error");
-        } 
-        response.json()
-    })
-       .then(data => console.log(data));
+       console.log(data);
        
-       document.getElementById("rezultat").innerHTML = "Requested results for : " + Input ;
+       document.querySelector("#rezultatHash").innerText = "Requested results for : " + data.malware.hash ;
+       document.querySelector("#rizikHash").innerText = "Risk level for this HASH is : " + data.malware.risk ;
+       document.getElementById("Hash1").value = "";
+    })
+
     }
        
 }
